5c8d2fd28158af6b733c4aea352e41f4b66fcf54
Julian Yap <jyap039@e.ntu.edu.sg>
2020-01-30 18:08:25 +0800

    Added Alpha 1.0

---
+++ app/src/main/java/lta/amazoning/track/ContentAdapter.java
@@ -0,0 +1,37 @@
+package lta.amazoning.track;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+/**
+ * A dummy {@link RecyclerView.Adapter} that displays a list of placeholder
+ * list items to the user.
+ */
+class ContentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final int ITEM_COUNT = 5;
+
+    private final LayoutInflater mInflater;
+
+    ContentAdapter(Context context) {
+        mInflater = LayoutInflater.from(context);
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new RecyclerView.ViewHolder(
+                mInflater.inflate(R.layout.view_holder_item, parent, false)) {};
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return ITEM_COUNT;
+    }
+}
