7fb59069669cb2586e8679da6d1c06f031c62453
Aaron Sng <aslw26@gmail.com>
2020-02-10 12:30:22 +0800

    Fixed network config

--- app/src/main/java/lta/amazoning/track/UploadFragment.java
+++ app/src/main/java/lta/amazoning/track/UploadFragment.java
@@ -6,6 +6,7 @@ import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
@@ -20,6 +21,7 @@ import androidx.fragment.app.Fragment;
 
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.security.NetworkSecurityPolicy;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
